38164cc1d513c9c7a55b97eb9493c102ea6cde3d
Cristobal-code <cristobalh517@gmail.com>
2023-09-01 15:23:34 -0400

    Adding custom response

--- src/main/java/cl/usm/hdd/calculadorarest/demo/services/CalculoService.java
+++ src/main/java/cl/usm/hdd/calculadorarest/demo/services/CalculoService.java
@@ -1,2 +1,9 @@
-package cl.usm.hdd.calculadorarest.demo.services;public interface CalculoService {
+package cl.usm.hdd.calculadorarest.demo.services;
+
+import cl.usm.hdd.calculadorarest.demo.exceptions.OperacionException;
+
+public interface CalculoService {
+     double calcular(int num1, int num2, String operacion) throws OperacionException;
+
+
 }
